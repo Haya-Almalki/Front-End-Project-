05e010a28ccc776ed245255b86deb0200d4e3ca8
Shahad Zam <shahaaz66@gmail.com>
2022-09-19 12:02:23 +0300

    adding profileP linked to backend

--- src/pages/ProfileP.js
+++ src/pages/ProfileP.js
@@ -1,33 +1,80 @@
-import { Input,Box,InputGroup,InputLeftAddon,HStack,Flex,VStack,Text,Button,Toast, useToast, Link, Image, Spacer,Select,Textarea,Divider} from '@chakra-ui/react'
+import { Input,Box,InputGroup,InputLeftAddon,HStack,Flex,VStack,Text,Button,Toast, useToast, Link, Image, Spacer,Select,Textarea,Divider,Spinner} from '@chakra-ui/react'
 import profile from '../images/profile.png';
 import Reviews from '../component/Reviews';
-import { useState } from 'react';
-import { StarIcon } from '@chakra-ui/icons'
+import { StarIcon} from '@chakra-ui/icons'
+import React, { useEffect, useState } from 'react';
+import { useLocation,useParams} from "react-router-dom";
+
 
 
 
 const ProfileP =()=>{
-const [reviewList,setReviewList]=useState([
-    {username:"Lubna",
-    message:"She was nice to work with",
-    rate:"5",},
-    {username:"Lubna",
-    message:"She was nice to work with",
-    rate:"5",},
-    {username:"Lubna",
-    message:"She was nice to work with",
-    rate:"5",},
-    {username:"Lubna",
-    message:"She was nice to work with",
-    rate:"5",},
-    {username:"Lubna",
-    message:"She was nice to work with",
-    rate:"5",},
-]);
+
+  const location = useLocation();
+  //let username = location.state.username;
+  //const username="as"
+  const { username } = useParams();
+  const [provider,setProvider]=useState()
+  const [loading, setLoading] = useState(true);
+  const [reviewList,setReviewList]=useState()
+
+
+
+
+
+
+  useEffect(() => {
+    const fetchPerson = async () => {
+      setLoading(true);
+      const request = await fetch('/api/v1/user/personByUsername/'+username);
+      const data = await request.json();
+      if (request.status === 200) {
+        console.log(data);
+        setProvider(data)
+        setLoading(false);
+      }
+    };
+    fetchPerson();
+  }, []);
+
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      setLoading(true);
+      const request = await fetch('/api/v1/review/reviewByPerson/'+username);
+      const data = await request.json();
+      if (request.status === 200) {
+        console.log(data);
+        setReviewList(data)
+        setLoading(false);
+      }
+    };
+    fetchReviews();
+  }, []);
+
+  
+
+
+
+
+
+
 
     const toast=useToast();
 
 return(
+  <>
+  {loading ? (
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="teal"
+          size="xl"
+        />
+      ) : (
+  
+  
     <HStack  width="100vw" height="100vh">
     <Flex
       height="100vh"
@@ -43,21 +90,34 @@ return(
         <Box align={"center"}>
          <Image src={profile}  width={"13rem"} height={"13rem"}/>
          <Text fontSize="2rem" color="#121440">
-          Provider name
+          {provider.personAndSkill.userBody.username}
         </Text>
          </Box>
          <Divider />
         <VStack spacing="2" align="left">
             <HStack>
             <Text fontWeight="bold">From: </Text>
-            <Text>London</Text>
+            <Text>{provider.personAndSkill.userBody.city}</Text>
             </HStack>
         <Divider />
             <Text fontSize={"1rem"} fontWeight="bold" > Description: </Text>
-          <Text>Radiant rank in Valorant, 3500 hours in csgo. Ex pro in fortnite. I have experience in competing and can help you improve whether it be your aim or game sense.</Text>
+          <Text>{provider.personAndSkill.userBody.description}</Text>
           <Divider />
           <Text fontSize={"1rem"} fontWeight="bold" > Skills: </Text>
-          <Text>Gaming</Text>
+          <Text>{provider.personAndSkill.skills.map((item)=> 
+           item.skill).join(", ")}</Text>
+          <Text fontSize={"1rem"} fontWeight="bold" > Average rate: </Text>
+          <Box display='flex' mt='2' alignItems='center'>
+          {Array(5)
+            .fill('')
+            .map((_, i) => (
+              <StarIcon
+                key={i}
+                w={6} h={6} 
+                color={i <provider.rate ? 'teal.500' : 'gray.300'}
+              />
+            ))}
+            </Box>
         </VStack>
       </VStack>
     </Flex>
@@ -70,23 +130,15 @@ return(
       >
         <VStack align={"left"} mt={"3rem"} width={"100%"}>
             <HStack>
-    <Text fontWeight="bold" fontSize={"2rem"} as='u'>Provider Reviews</Text>
-    <Box display='flex' mt='2' alignItems='center'>
-          {Array(5)
-            .fill('')
-            .map((_, i) => (
-              <StarIcon
-                key={i}
-                w={6} h={6} 
-                color={i <4 ? 'teal.500' : 'gray.300'}
-              />
-            ))}
-            </Box>
+    <Text mb={"10"} ml={"400"} fontWeight="bold" fontSize={"2rem"} >{provider.personAndSkill.userBody.username} Reviews</Text>
+   
     </HStack>
     <Reviews reviewList={reviewList}/>
       </VStack>
     </Flex>
   </HStack>
+      )}
+      </>
 )
 
 
